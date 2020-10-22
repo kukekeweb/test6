d523120fa8476fc3674655dc63c4a31f83664369
kukekeweb <73024063+kukekeweb@users.noreply.github.com>
2020-10-22 20:44:02 +0900

    Add files via upload

---
+++ my6.js
@@ -0,0 +1,10 @@
+function play_se(){
+    var フフフフフォートナイトDNBREMIX = new Audio('フフフフフォートナイトDNBREMIX.mp3');
+    フフフフフォートナイトDNBREMIX.play();
+}
+
+
+$(function(){
+    $('#6曲目').click(play_se);
+});
+
